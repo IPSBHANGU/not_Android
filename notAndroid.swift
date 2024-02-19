3549eeb4c73a8fe0da95234a7879e0109ac0f9ac
Inderpreet Singh <inderpreetbhangu2@gmail.com>
2024-02-19 16:16:36 +0530

    switch to Result Return type

--- notAndroid.swift
+++ notAndroid.swift
@@ -1,14 +1,14 @@
 class notAndroid<T> {
-      func deleteDataObjectCommon(model: NSManagedObject) -> (Bool, String) {
+    func deleteDataObjectCommon(model: NSManagedObject) -> Result<Bool,Error> {
         if let context = appDelegate?.persistentContainer.viewContext {
             context.delete(model)
             do {
                 try context.save()
-                return (true, "Objects Deletion Success!")
+                return .success(true)
             } catch {
                 print("Error while deleting Teacher profile! \(error.localizedDescription)")
             }
         }
-        return (false, "Context is nil")
+        return .failure("Failed to Delete Object" as! Error )
     }
 }
